91f490ae18c216a76d1d25437f5eb998ce2434aa
Branko Mirkovic <57894223+BrankoMirkovicFEDEV@users.noreply.github.com>
2023-10-30 14:31:12 +0100

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,12 @@
+// models/user.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+});
+
+const UserModel = mongoose.model("User", userSchema);
+
+module.exports = UserModel;
