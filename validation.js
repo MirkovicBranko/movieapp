dc8c2d5b516ecfe0088eeda2e05feac8338a63bd
Branko Mirkovic <57894223+BrankoMirkovicFEDEV@users.noreply.github.com>
2023-11-01 19:17:55 +0100

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,22 @@
+// validators.js
+
+//function for email formating
+function isValidEmail(email) {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegex.test(email);
+}
+
+// Function for username formating
+function isValidUsername(username) {
+    const usernameRegex = /^[a-zA-Z0-9_]+$/;
+    return usernameRegex.test(username);
+}
+
+// Function for password formating
+function isValidPassword(password) {
+    // Password has to include atleast 8 characters, including atleast one small letter, one big letter and one number.
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    return passwordRegex.test(password);
+}
+
+module.exports = { isValidEmail, isValidUsername, isValidPassword };
